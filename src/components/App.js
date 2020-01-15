9399a7ff3c3d7007599606708d95df137062ce58
ashutosh-tomilkieway <anc-datapipelines/ashutosh@tomilkieway.com>
2020-01-15 15:47:52 +0530

    added

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react';
-import Web3 from 'web3';
-import './App.css';
-import Meme from '../abis/Meme.json'
+import React, { Component, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getCurrentProfile } from "../actions/profile";
+//import Web3 from "web3";
+import "./App.css";
+//import Meme from "../abis/Meme.json";
 
-const ipfsClient = require('ipfs-http-client')
-const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' }) // leaving out the arguments will default to these values
-
-class App extends Component {
-
+//const ipfsClient = require("ipfs-http-client");
+import ipfs from "./ipfs";
+/*const ipfs = ipfsClient({
+  host: "ipfs.infura.io",
+  port: 5001,
+  protocol: "https"
+}); // leaving out the arguments will default to these values
+*/
+const App = ({ getCurrentProfie, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfie();
+  }, []);
+  var state = { buffer: null };
+  /*
   async componentWillMount() {
     await this.loadWeb3()
     await this.loadBlockchainData()
@@ -42,82 +54,85 @@ class App extends Component {
       window.alert('Smart contract not deployed to detected network.')
     }
   }
+*/
 
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      memeHash: '',
-      contract: null,
-      web3: null,
-      buffer: null,
-      account: null
-    }
-  }
-
-  captureFile = (event) => {
-    event.preventDefault()
-    const file = event.target.files[0]
-    const reader = new window.FileReader()
-    reader.readAsArrayBuffer(file)
+  const captureFile = event => {
+    event.preventDefault();
+    const file = event.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
     reader.onloadend = () => {
-      this.setState({ buffer: Buffer(reader.result) })
-      console.log('buffer', this.state.buffer)
-    }
-  }
+      this.setState({ buffer: Buffer(reader.result) });
+      console.log("buffer", this.state.buffer);
+    };
+  };
 
-  onSubmit = (event) => {
-    event.preventDefault()
-    console.log("Submitting file to ipfs...")
-    ipfs.add(this.state.buffer, (error, result) => {
-      console.log('Ipfs result', result)
-      if(error) {
-        console.error(error)
-        return
+  const onSubmit = event => {
+    event.preventDefault();
+    console.log("Submitting file to ipfs...");
+    console.log("buffer", this.state.buffer);
+    ipfs.files.add(this.state.buffer, (error, result) => {
+      if (error) {
+        console.error(error);
+        return;
       }
-       this.state.contract.methods.set(result[0].hash).send({ from: this.state.account }).then((r) => {
-         return this.setState({ memeHash: result[0].hash })
-       })
-    })
-  }
+      /*   this.state.contract.methods
+        .set(result[0].hash)
+        .send({ from: this.state.account })
+        .then(r => {
+          return this.setState({ memeHash: result[0].hash });
+        });
+      */
+      const va = result[0].hash;
+      console.log("Ipfs result", va);
+    });
+  };
 
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Meme of the Day
-          </a>
-        </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={`https://ipfs.infura.io/ipfs/${this.state.memeHash}`} />
-                </a>
-                <p>&nbsp;</p>
-                <h2>Change Meme</h2>
-                <form onSubmit={this.onSubmit} >
-                  <input type='file' onChange={this.captureFile} />
-                  <input type='submit' />
-                </form>
-              </div>
-            </main>
-          </div>
+  return (
+    <div>
+      <nav className='navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow'>
+        <a
+          className='navbar-brand col-sm-3 col-md-2 mr-0'
+          href='http://www.dappuniversity.com/bootcamp'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Meme of the Day
+        </a>
+      </nav>
+      <div className='container-fluid mt-5'>
+        <div className='row'>
+          <main role='main' className='col-lg-12 d-flex text-center'>
+            <div className='content mr-auto ml-auto'>
+              <a
+                href='http://www.dappuniversity.com/bootcamp'
+                target='_blank'
+                rel='noopener noreferrer'
+              ></a>
+              <p>&nbsp;</p>
+
+              <h2>Change Meme</h2>
+              <form onSubmit={this.onSubmit}>
+                <input type='file' onChange={this.captureFile} />
+                <input type='submit' />
+              </form>
+            </div>
+          </main>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default App;
+App.propTypes = {
+  getCurrentProfie: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profile
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(App);
